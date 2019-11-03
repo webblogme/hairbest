9c592b794fd4cb91d0bd640d10133923fb61e391
webblogme <webblogme@gmail.com>
2019-11-04 01:34:45 +0700

    adjust top menu / remove top stripe becuase they are repeat in production - dont know why...

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div>
-    <div class="something">
-      <div class="container" style="background:none; box-shadow:none;">
-        <font-awesome-icon icon="star"/>&nbsp; ร้านเสริมสวย "แฮร์เบสท์" ถนนเจ้าฟ้าตะวันออก (ตรงข้ามตลาดมุมเมือง)
-      </div>
-    </div>
     <div id="app" class="container">
       <router-view/>
     </div>
